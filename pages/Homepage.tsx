2d7c11c38c763bcb56312a42db7fdee12f169b6f
Ryan McKevitt <mckevitt.r@gmail.com>
2024-10-24 23:39:06 -0500

    initialized temporary homepage

---
+++ pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+    return (
+        <div className={"temp-landing"}>
+            <h5>oceanum<br/>group</h5>
+        </div>
+    );
+};
+
+export default Homepage;
